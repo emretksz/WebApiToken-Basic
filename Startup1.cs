8cbf120e11393f64deaaad0ec5bfc39bd60799ea
Emre Toksöz <emretkszz@gmail.com>
2022-02-12 22:14:53 +0300

    Startup Eklendi

---
+++ Startup1.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(AspNetBolumler.App_Start.Startup1))]
+
+namespace AspNetBolumler.App_Start
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            ConfigureOAuth(app);
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
+        }
+
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthAuthorizationServerOptions = new OAuthAuthorizationServerOptions
+            {
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                AllowInsecureHttp = true,
+                Provider = new SimpleAuthorizationServerProvider()
+
+            };
+
+            app.UseOAuthAuthorizationServer(OAuthAuthorizationServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+        }
+        public class SimpleAuthorizationServerProvider:OAuthAuthorizationServerProvider
+        {
+
+            public override Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+            {
+                context.Validated();
+                return Task.FromResult<object>(null);
+            }
+            
+            public override Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+            {
+                context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
+
+                if (context.UserName=="Emre"&& context.Password=="123")
+                {
+                    var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+                    identity.AddClaim(new Claim("sub", context.UserName));
+                    identity.AddClaim(new Claim("role", "user"));
+                    context.Validated(identity);
+                }
+                else
+                {
+                    context.SetError("invalid_grant", "Hatalı Giriş");
+                }
+
+                return Task.FromResult<object>(null);
+            }
+        }
+    }
+}
---
+++ TokenApiController.cs
@@ -0,0 +1,48 @@
+﻿using AspNetBolumler.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+namespace AspNetBolumler.Controllers
+{
+       [Authorize]
+    public class TokenApiController : ApiController
+    {
+        Models.model1 db = new Models.model1();
+        [HttpGet]
+        public IQueryable<Employees> Calisanlar()
+        {
+            return db.Employees;
+        }
+
+        [HttpPost]
+        public IHttpActionResult CalisanEkle(Employees employees)
+        {
+            db.Employees.Add(employees);
+            db.SaveChanges();
+            return Json("Ekleme Başarılı");
+        }
+
+        [HttpPut]
+        public IHttpActionResult CalisanGuncelle(Employees employees)
+        {
+            db.Entry(employees).State = EntityState.Modified;
+            db.SaveChanges();
+            return Json("Güncelleme başarılı");
+        }
+       [HttpDelete]
+       public IHttpActionResult CalisanSil(int id)
+        {
+            var sil = db.Employees.FirstOrDefault(m => m.EmployeeID == id);
+            db.Employees.Remove(sil);
+            db.SaveChanges();
+            return Json("Başarılı");
+        }
+
+    }
+}
